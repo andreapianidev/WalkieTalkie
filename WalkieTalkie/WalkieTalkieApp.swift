32c60eb72e696bf69640b35f9b8de2a47c21c7f5
Andrea Piani <andreapiani.dev@gmail.com>
2025-07-12 19:14:08 +0100

    Initial Commit

---
+++ WalkieTalkie/WalkieTalkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkieTalkieApp.swift
+//  WalkieTalkie
+//
+//  Created by Andrea Piani on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct WalkieTalkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
